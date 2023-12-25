2e76be827c58340f4d23c93d7260217b82fe3231
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-25 18:52:47 +0300

    Update cmp_03.cpp

--- comparison/cmp_03.cpp
+++ comparison/cmp_03.cpp
@@ -4,11 +4,11 @@
 int main()
 {
 	std::optional<int> op1;  //empty durumda
-	std::optional<int> op2{ 2 };
-	std::optional<int> op3{ 3 };
+	std::optional op2{ 2 };
+	std::optional op3{ 3 };
 
-	std::cout << std::boolalpha;
-	std::cout << (op1 > op2) << '\n'; 
+	boolalpha(std::cout);
+	std::cout << (op1 > op2) << '\n';
 	std::cout << (op2 < op3) << '\n';
 	std::cout << (op1 < op2) << '\n';
 	std::cout << (op1 == std::nullopt) << '\n';
