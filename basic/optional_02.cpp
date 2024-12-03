5f19eed7a55f3ac951c1a029bb7dabebee4fddfe
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-03 09:59:37 +0300

    Create optional_02.cpp

---
+++ basic/optional_02.cpp
@@ -0,0 +1,13 @@
+#include <optional>
+#include <iostream>
+
+int main()
+{
+	using std::optional, std::cout;
+
+	cout << sizeof(optional<char>) << ' ';
+	cout << sizeof(optional<int>) << ' ';
+	cout << sizeof(optional<double>) << '\n';
+}
+
+// sample output: 2	 8 16
